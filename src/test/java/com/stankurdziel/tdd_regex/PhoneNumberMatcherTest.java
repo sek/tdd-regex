1dffc207288cf4a32427487f3e30340dfd88a222
Stanley Kurdziel <Stanley.Kurdziel@ResMed.com>
2018-10-21 22:11:35 -0700

    Add negative case - passes with no change

--- src/test/java/com/stankurdziel/tdd_regex/PhoneNumberMatcherTest.java
+++ src/test/java/com/stankurdziel/tdd_regex/PhoneNumberMatcherTest.java
@@ -1,5 +1,6 @@
 package com.stankurdziel.tdd_regex;
 
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
@@ -9,5 +10,7 @@ public class PhoneNumberMatcherTest {
     public void shouldMatch10DigitPhoneNumbers() {
         assertTrue(new PhoneNumberMatcher("858-555-1212").isValid());
         assertTrue(new PhoneNumberMatcher("(858)555-1212").isValid());
+        assertFalse(new PhoneNumberMatcher("858-555-333").isValid());
+    }
     }
 }
