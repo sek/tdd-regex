d199ab4bd3b1517f03857d5340fefffee45fe430
Stanley Kurdziel <Stanley.Kurdziel@ResMed.com>
2018-10-21 22:50:50 -0700

    Add test to get area code

--- src/test/java/com/stankurdziel/tdd_regex/PhoneNumberMatcherTest.java
+++ src/test/java/com/stankurdziel/tdd_regex/PhoneNumberMatcherTest.java
@@ -1,24 +1,38 @@
 package com.stankurdziel.tdd_regex;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
+
 import org.junit.Test;
 
 public class PhoneNumberMatcherTest {
 
     @Test
     public void shouldMatch10DigitPhoneNumbers() {
-        assertTrue(new PhoneNumberMatcher("858-555-1212").isValid());
-        assertTrue(new PhoneNumberMatcher("(858)555-1212").isValid());
-        assertFalse(new PhoneNumberMatcher("858-555-333").isValid());
+        assertTrue(m("858-555-1212").isValid());
+        assertTrue(m("(858)555-1212").isValid());
+        assertFalse(m("858-555-333").isValid());
     }
 
     @Test
     public void shouldMatchCountryCode() {
-        assertTrue(new PhoneNumberMatcher("+1 858-555-1212").isValid());
-        assertTrue(new PhoneNumberMatcher("+01 858-555-1212").isValid());
-        assertTrue(new PhoneNumberMatcher("+1-858-555-1212").isValid());
-        assertTrue(new PhoneNumberMatcher("+1(858)555-1212").isValid());
-        assertFalse(new PhoneNumberMatcher("1(858)555-1212").isValid());
+        assertTrue(m("+1 858-555-1212").isValid());
+        assertTrue(m("+01 858-555-1212").isValid());
+        assertTrue(m("+1-858-555-1212").isValid());
+        assertTrue(m("+1(858)555-1212").isValid());
+        assertFalse(m("1(858)555-1212").isValid());
+    }
+
+    private static PhoneNumberMatcher m(String number) {
+        return new PhoneNumberMatcher(number);
+    }
+
+    @Test
+    public void shouldRetrieveAreaCode() {
+        assertEquals("858", m("+1 858-555-1212").getAreaCode());
+        assertEquals("619", m("+01 619-555-1212").getAreaCode());
+        assertEquals("760", m("+1-760-555-1212").getAreaCode());
+        assertEquals("123", m("+1(123)555-1212").getAreaCode());
     }
 }
